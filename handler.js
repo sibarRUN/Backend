2bae4d13cf82d4a6504f96f7efc5485b08671880
ddoljugy <ddoljugy@gmail.com>
2024-12-03 17:10:44 +0900

    v 1.5.2

--- handler.js
+++ handler.js
@@ -1,17 +1,14 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors'); // CORS 미들웨어 가져오기
-const registerRoutes = require('./src/api/register'); // register API 가져오기
 const bongjiniRoutes = require('./src/api/bongjini'); // bongjini API 가져오기
-
 
 const app = express();
 app.use(cors()); // CORS 문제 해결을 위해 추가
 app.use(express.json()); // JSON 요청을 처리
 
 // API 경로 설정
-app.use('/register', registerRoutes); // /register API 연결
-app.use('/bongjini', bongjiniRoutes); // /bongjini API 연결
+app.use('/bongjini', bongjiniRoutes); // /bongjini API 연결 (사용자 키, 체중 정보 관련)
 
 // Lambda 핸들러로 앱 변환
 module.exports.handler = serverless(app);
---
+++ server.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const cors = require('cors');
+// const bongjiniRoutes = require('./src/api/bongjini'); // bongjini API 가져오기
+
+// const app = express();
+// app.use(cors()); // 모든 출처에서의 CORS 요청 허용
+// app.use(express.json()); // JSON 요청을 처리
+
+// // bongjini 라우터를 등록
+// app.use('/bongjini', bongjiniRoutes); // /bongjini API 연결 (사용자 키, 체중 정보 관련)
+
+// // 로컬 서버 실행
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
